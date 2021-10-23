4e474582233649d2fd8efedb61f45e968055a103
Osman <said.osman7991@gmail.com>
2021-10-23 18:37:40 +0200

    Numbers and Strings in python

--- lab_01/lab01_network.py
+++ lab_01/lab01_network.py
@@ -2,3 +2,35 @@
 #printing hello world and exploring Python!
 
 print("Hello world!")
+
+####  Numbers ####
+tax = 12 / 100
+# division always returns a floating point number
+price = 100
+print ("printing some numbers")
+print ( tax * price )
+
+
+#### Strings ####
+a = "String using double quotes"
+b = 'String using single quotes'
+c = 'Use \' to escape quotes'
+print(a)
+print(b)
+print(a + b)
+
+# String literals can span multiple lines. We can use it with triple-quotes
+print('''
+Usage: thingy [OPTIONS]
+	-h			Display this usage message
+	-H hostname		Hostname to connect to
+''')
+
+# To concatenate strings use + operator. To repeat strings use * operator
+hello = 'hello '
+world = 'world'
+print(2 * hello + world)
+
+print('Put several strings within parantheses'
+	'to have them joined')
+
