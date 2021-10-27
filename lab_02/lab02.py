b43eda7f73140bfd349ffdeceee12ec448c92d57
Umid0vic <57035358+Umid0vic@users.noreply.github.com>
2021-10-27 14:42:43 +0200

    split and store the rows from a file into dictionaries

---
+++ lab_02/lab02.py
@@ -0,0 +1,18 @@
+#lab 2 - Network Programming
+#created by Osman Said 2021-10-26
+
+dic = {}
+lista = []
+file = open('score2.txt')
+
+for line in file.readlines():
+    upp, number, firstName, lastName, points = line.split()
+    key = firstName+lastName
+    if key in dic:
+    	new_points = dic[key] + int(points)
+    	dic[key] = new_points
+    else:
+    	dic[firstName+ ' ' +lastName] = int(points)
+
+print(dic)
+
