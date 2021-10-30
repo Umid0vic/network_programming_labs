b6c7a165d292d4197aa84b15aecedd68217ca49b
Osman <said.osman7991@gmail.com>
2021-10-31 00:45:04 +0200

    class Card implemented

--- lab_03/lab_03.py
+++ lab_03/lab_03.py
@@ -7,22 +7,35 @@ class Card:
 		assert 1 <= suit <= 4 and 1 <= value <= 13
 		self._suit = suit
 		self._value = value
+	
 	def getValue(self):
-		pass
+		return self._value
+	
 	def getSuit(self):
-		pass
+		return self._suit
+	
 	def __str__(self):
-		pass
+		values = [ 'Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
+			'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']			]
 
+		suits = ['Spades','Hearts','Diamons','Clubs']
+		
+		value = values[self.getValue() + 1]
+		suit  = suits[self.getSuit() + 1]
+		return '{} of {}'.format(value, suit)
 
 class CardDeck:
 	def __init__(self):
 		pass
+	
 	def shuffle(self):
 		pass
+	
 	def getCard(self):
 		pass
+	
 	def size(self):
 		pass
+	
 	def reset(self):
 		pass
