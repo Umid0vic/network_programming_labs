16ccd6ae313fa090bc849b64e9f23aa7b1a38f23
Osman <said.osman7991@gmail.com>
2021-11-09 22:51:51 +0100

    created socket object

--- lab_06/lab_06.py
+++ lab_06/lab_06.py
@@ -1,2 +1,9 @@
 # Network programming lab 6 - HHTP request
 # craeted by Osman Said 07-11-2021
+
+port = 8080
+host = ''
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((host, port))
+
+#establish connection with client
