0e02f81835bc27121c52eef3f4d39bac9093d6ee
Osman <said.osman7991@gmail.com>
2021-11-15 22:40:20 +0100

    accepting data from client

--- lab_06/lab_06.py
+++ lab_06/lab_06.py
@@ -1,5 +1,7 @@
 # Network programming lab 6 - HHTP request
 # craeted by Osman Said 07-11-2021
+import socket
+
 
 port = 8080
 host = ''
@@ -7,3 +9,23 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((host, port))
 
 #establish connection with client
+while True:
+	print('listening...')
+	c, addr = sock.accept()
+	data = c.recv(1024)
+	data = data.decode()
+
+	print(data)
+	c.sendall(bytearray('HTTP/1.1 200 ok\n', 'ASCII'))
+	c.sendall(bytearray('''
+		<html>
+		<body>
+		<h1>Your browser sent the following request:</h1>
+		<pre>''' , 'ASCII'))
+	c.send(bytearray(data, 'ASCII'))
+	c.send(bytearray('''
+		</pre>
+		</body>
+		</html>''', 'ASCII'))
+	c.close()
+sock.close()
